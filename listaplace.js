c4856d288ff07cbb4e2eb56c92c113b4bdb83df7
damodar344 <114390278+damodar344@users.noreply.github.com>
2025-05-06 13:13:55 -0400

    Delete listaplace.js

--- listaplace.js
+++
@@ -1,5 +0,0 @@
-const welcomeMessage = document.getElementById('welcome-message');
-
-function setWelcomeUserName(userName) {
-    welcomeMessage.innerHTML = `Welcome to ShareIT, ${userName}! Let’s help you find your ideal roommate`;
-}
